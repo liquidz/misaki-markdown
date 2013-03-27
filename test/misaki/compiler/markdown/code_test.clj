bb126e78bc0c8e0d590032a14bd52d9a3759859b
liquidz <liquidz.uo@gmail.com>
2013-03-28 07:54:19 +0900

    add test pattern: misaki.compiler.markdown.code

--- test/misaki/compiler/markdown/code_test.clj
+++ test/misaki/compiler/markdown/code_test.clj
@@ -2,11 +2,9 @@
   (:require
     [misaki.compiler.markdown [code :refer :all]]
     [misaki [config :refer [*config*]]
-            [tester :refer :all]
-            ]
-    [clojure.test :refer :all]
-    [clojure.string :as str]))
-
+            [tester :refer :all]]
+    [clojure [test   :refer :all]
+             [string :as str]]))
 
 ;; gen-uniq-str
 (deftest gen-uniq-str-test
@@ -17,32 +15,41 @@
 (defcompilertest convert-without-codes-test
   (let [f #(str/replace % #"a" "A")]
 
-    (are [x y] (= x (convert-without-codes f y))
-      "AAA" "aaa"
-      "AAA\nbbb" "aaa\nbbb"
-      "AA```\naaa\n```"  "aa```\naaa\n```"
-      "AA```\naaa\nbbb\n```"  "aa```\naaa\nbbb\n```"
-      "AA```\naaa\nbbb\n\n```"  "aa```\naaa\nbbb\n\n```")
+    (testing "code block without language"
+      (are [x y] (= x (convert-without-codes f y))
+        "AAA"                   ,  "aaa"
+        "AAA\nbbb"              ,  "aaa\nbbb"
+        "AA```\naaa\n```"       ,  "aa```\naaa\n```"
+        "AA```\naaa\nbbb\n```"  ,  "aa```\naaa\nbbb\n```"
+        "AA```\naaa\nbbb\n\n```",  "aa```\naaa\nbbb\n\n```"))
 
-    (are [x y] (= x (convert-without-codes f y))
-      "AA```lang\naaa\n```" "aa```lang\naaa\n```")
+    (testing "code block with language"
+      (are [x y] (= x (convert-without-codes f y))
+        "AA```lang\naaa\n```"       ,  "aa```lang\naaa\n```"
+        "AA```lang\naaa\nbbb\n```"  ,  "aa```lang\naaa\nbbb\n```"
+        "AA```lang\naaa\nbbb\n\n```",  "aa```lang\naaa\nbbb\n\n```"))
 
-    (are [x y] (= x (convert-without-codes f y))
-      "```\na\n``` A ```\na\n```" "```\na\n``` a ```\na\n```")
-    ))
+    (testing "multiple code blocks"
+      (are [x y] (= x (convert-without-codes f y))
+        "```\na\n``` A ```\na\n```"    , "```\na\n``` a ```\na\n```"
+        "```\na\n``` A ```lang\na\n```", "```\na\n``` a ```lang\na\n```"))))
 
 ;; transform-codes
 (defcompilertest transform-codes-test
-  (are [x y] (= x (transform-codes y))
-    "<pre><code>a</code></pre>" "```\na\n```"
-    "<pre><code>a</code></pre>" "```\n\n\na\n\n\n```"
-    "<pre><code>a\nb</code></pre>" "```\na\nb\n```"
-    )
+  (testing "transform code without language"
+    (are [x y] (= x (transform-codes y))
+      "<pre><code>a</code></pre>"   , "```\na\n```"
+      "<pre><code>a</code></pre>"   , "```\n\n\na\n\n\n```"
+      "<pre><code>a\nb</code></pre>", "```\na\nb\n```"))
 
-  (are [x y] (= x (transform-codes y))
-    "<pre><code class=\"brush: clj;\">a</code></pre>" "```clj\na\n```"
-    "<pre><code class=\"brush: clj;\">a\nb</code></pre>" "```clj\na\nb\n```"
-    )
-  )
+  (testing "transform code with language"
+    (are [x y] (= x (transform-codes y))
+      "<pre><code class=\"brush: clj;\">a</code></pre>"   , "```clj\na\n```"
+      "<pre><code class=\"brush: clj;\">a\nb</code></pre>", "```clj\na\nb\n```"))
+
+  (testing "transform multiple codes"
+    (are [x y] (= x (transform-codes y))
+      "<pre><code>a</code></pre> b <pre><code>c</code></pre>", "```\na\n``` b ```\nc\n```"
+      "<pre><code>a</code></pre> b <pre><code class=\"brush: lang;\">c</code></pre>", "```\na\n``` b ```lang\nc\n```")))
 
 
